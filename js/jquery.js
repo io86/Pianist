b545802b8a209efe46d78d473a744d8c78de5257
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-02-27 23:15:07 +0100

    Add files via upload
    Upload jquery file

--- js/jquery.js
+++ js/jquery.js
@@ -55,19 +55,21 @@ $(document).ready(function()  {
 	});
 	
 	//enquiry
-	$('.enquiry').mouseenter(function()  {
-		$(this).css("cursor", "pointer");
-	});
-	$('.enquiry').click(function()  {
-		if (widthWindow < 450)  {
-			$('html, body').animate({scrollTop: 3150}, 800);
-			return false;
-		} else if (widthWindow < 600)  {
-			$('html, body').animate({scrollTop: 2950}, 800);
-			return false;
-		} else {
-			$('html, body').animate({scrollTop: 2800}, 800);
-			return false;
-		}
+	$('.enquiry').on({
+		mouseenter: function()  {
+		    $(this).css("cursor", "pointer");
+	},
+	    click: function()  {
+		    if (widthWindow < 450)  {
+			    $('html, body').animate({scrollTop: 3150}, 800);
+			    return false;
+		    } else if (widthWindow < 600)  {
+			    $('html, body').animate({scrollTop: 2950}, 800);
+			    return false;
+		    } else {
+			    $('html, body').animate({scrollTop: 2800}, 800);
+			    return false;
+		    }
+	    }
 	});
 });
