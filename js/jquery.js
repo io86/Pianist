7d3abc0d223b5c72e069d54d0a664f5b3f901ebd
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-03-12 12:15:09 +0100

    Add files via upload
    Upload jQuery file

--- js/jquery.js
+++ js/jquery.js
@@ -21,7 +21,7 @@ $(document).ready(function()  {
 			$('html, body').animate({scrollTop: 1900}, 800);
 			return false;
 	    } else {
-			$('html, body').animate({scrollTop: 1700}, 800);
+			$('html, body').animate({scrollTop: 1800}, 800);
 			return false;
 		}
 	});
@@ -29,19 +29,36 @@ $(document).ready(function()  {
 	//Scroll Availlability
 	$('#availlability-li').click(function()  {
 	    if (widthWindow < 450)  {
-			$('html, body').animate({scrollTop: 2600}, 800);
+			$('html, body').animate({scrollTop: 2880}, 800);
 			return false;
 		} else if (widthWindow < 600)  {
-			$('html, body').animate({scrollTop: 2400}, 800);
+			$('html, body').animate({scrollTop: 2700}, 800);
 			return false;
 		} else {
-			$('html, body').animate({scrollTop: 2200}, 800);
+			$('html, body').animate({scrollTop: 2600}, 800);
 			return false;
 		}
 	});
 	
 	//Scroll Contact
 	$('#contact-li').click(function()  {
+		if (widthWindow < 450)  {
+			$('html, body').animate({scrollTop: 3380}, 800);
+			return false;
+		} else if (widthWindow < 600)  {
+			$('html, body').animate({scrollTop: 3200}, 800);
+			return false;
+		} else {
+			$('html, body').animate({scrollTop: 3100}, 800);
+			return false;
+		}
+	});
+	
+	//enquiry
+	$('.enquiry').mouseenter(function()  {
+		$(this).css("cursor", "pointer");
+	});
+	$('.enquiry').click(function()  {
 		if (widthWindow < 450)  {
 			$('html, body').animate({scrollTop: 3150}, 800);
 			return false;
@@ -52,24 +69,5 @@ $(document).ready(function()  {
 			$('html, body').animate({scrollTop: 2800}, 800);
 			return false;
 		}
-	});
-	
-	//enquiry
-	$('.enquiry').on({
-		mouseenter: function()  {
-		    $(this).css("cursor", "pointer");
-	},
-	    click: function()  {
-		    if (widthWindow < 450)  {
-			    $('html, body').animate({scrollTop: 3150}, 800);
-			    return false;
-		    } else if (widthWindow < 600)  {
-			    $('html, body').animate({scrollTop: 2950}, 800);
-			    return false;
-		    } else {
-			    $('html, body').animate({scrollTop: 2800}, 800);
-			    return false;
-		    }
-	    }
 	});
 });
