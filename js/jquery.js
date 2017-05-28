538f0ec2e755be340617446077d9a18635e54a60
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-05-28 17:36:49 +0200

    Add files via upload
    Upload script file

--- js/jquery.js
+++ js/jquery.js
@@ -1,48 +1,36 @@
-$(document).ready(function()  {
+$(document).ready(function () {
 	
-	//var headerH = $('#wraper #header').outerHeight();
-	//var bioH = $('main .bio').outerHeight();
-	//var listenH = $('main .listen').outerHeight();
-	//var menuH = $('nav#menu').outerHeight();
-	 
-	alert($('#bio').offset().top);
-	alert($('#listen').offset().top);
-	alert($('#contact').offset().top);
+	function scrolledArea(el) {
+		var elOffset = el.offset(),
+		    elOffsetTop = elOffset.top;
+			
+		$('html, body').animate({scrollTop: elOffsetTop}, 800);
+	}
 	
 	//Scroll Bio
-	$('#menu #bio-li').click(function()  {
-		var bioOffset = $("#bio").offset(),
-		bioOffsetTop = bioOffset.top;
-		
-		$('html, body').animate({scrollTop : bioOffsetTop}, 800);
+	$('#bio-li').click(function () {
+	    scrolledArea($('#bio'));
 	});
 	
 	//Scroll Listen
-	$('#menu #listen-li').click(function()  {
-        var listenOffset = $('#listen').offset();
-		var listenOffsetTop = listenOffset.top;
-	
-		$('html, body').animate({scrollTop: listenOffsetTop}, 800);
+	$('#listen-li').click(function () {
+	    scrolledArea($('#listen'));
 	});
 	
 	//Scroll Contact
-	$('#menu #cont-li').click(function()  {
-		var contactOffset = $('#contact').offset();
-		var contactOffsetTop = contactOffset.top;
-		
-		$('html, body').animate({scrollTop: contactOffsetTop}, 800);
+	$('#cont-li').click(function () {
+	    scrolledArea($('#contact'));
 	});
 	
 	//enquiry
-	$('p .enquiry').click(function()  {
-		var contactOffset = $('#contact').offset();
-		var contactOffsetTop = contactOffset.top;
-		
-		$('html, body').animate({scrollTop: contactOffsetTop}, 800);
+	$('#enquir').click(function () {
+		scrolledArea($('#contact'));
 	});
 	
-	//enquiry
-	$('i#top').click(function()  {
-		$('html, body').animate({scrollTop: 0}, 800);
+	//Scrrol to top
+	$('i#top').click(function () {
+		scrolledArea($('body'));
 	});
+	
+	
 });
