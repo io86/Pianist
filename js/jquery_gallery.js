2fcbda2b45a2bb457547df871c835ba361fac184
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-08-08 23:58:27 +0200

    Add script for gallery.html

---
+++ js/jquery_gallery.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    //Array with src of photoes
+	var srcArray = ['../img/gallery_1.jpg', '../img/gallery_2.jpg', '../img/gallery_3.jpg', '../img/gallery_4.jpg', '../img/gallery_5.jpg', '../img/gallery_6.jpg', '../img/gallery_7.jpg', '../img/gallery_8.jpg', '../img/gallery_9.jpg'];
+    
+    //Display image that is clicked
+    $('.inside-main img').click(function () {
+
+        var src = $(this).attr('src');
+        
+        $('.modal-box').css('display', 'block');
+        
+        $('.modal-box img').attr('src', src);
+        
+    });
+    
+    //Change image when is clicked
+    $('.modal-box img').click(function () {
+        var srcActive = $('.modal-box img').attr('src'),
+            x = srcArray.indexOf(srcActive);
+        
+        if(x === 8) {
+            x = -1;
+        }
+        
+        $('.modal-box img').attr('src', srcArray[x+1]);
+
+    });
+    
+    //Hover effect on image
+    $('.frame-img img').hover(function () {
+        $(this).css('box-shadow', '2px 2px 10px #000000'); 
+    },
+    function() {
+        $(this).css('box-shadow', 'none'); 
+    });
+    
+    //Close modal-box
+    $('.close').click(function () {
+        $('.modal-box').css('display', 'none'); 
+    });
+    
+});
