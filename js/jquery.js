754f094a010cf0e61f6327491f49c3fdbd537e0d
Ioanna Mitroulaki <iomitroulaki@yahoo.gr>
2017-08-08 14:28:12 +0200

    Add script file

---
+++ js/jquery.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+	
+    $('.btn-menu').click(function () {
+        $('.navi').slideToggle();
+        
+        $('.line-btn:nth-child(1)').toggle();
+        
+        $('.line-btn:nth-child(2)').toggleClass('rotate-first');
+        $('.line-btn:nth-child(3)').toggleClass('rotate-second');
+    });
+    
+    //Title under the image profil is appeared
+    var $h1Title = $('.title h1'),
+        $h2Title = $('.title h2'),
+        $imgProfil = $('.img-profil');
+    
+    function title(x, y) {
+        x.delay(y).fadeIn();
+    }
+    
+
+    title($h1Title, 500);
+    title($h2Title, 1000);
+	
+    //Scroll until the appropriate area
+    function scrolledArea(el) {
+        var elOffset = el.offset().top;
+        
+        $('html, body').animate({scrollTop: elOffset}, 800);
+    }
+    
+    $('.about-mobile, .about-black').click(function () {
+        scrolledArea($('.about')); 
+    });
+    
+    $('.concerts-mobile, .concerts-black').click(function () {
+        scrolledArea($('.concerts')); 
+    });
+    
+    $('.calendar-mobile, .calendar-black').click(function () {
+        scrolledArea($('.availability')); 
+    });
+    
+    $('.contact-mobile, .contact-black').click(function () {
+        scrolledArea($('.contact')); 
+    });
+    
+    $('.green').click(function () {
+        scrolledArea($('.contact'));
+    });
+    
+    $('.back-top').click(function () {
+        scrolledArea($('header')); 
+    });
+	
+});
